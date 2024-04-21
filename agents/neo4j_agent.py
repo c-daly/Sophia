87d431c52da775a1138d2bd5af6f954d2f0271bd
c-daly <chris@cdaly.me>
2024-04-21 06:22:25 -0400

    added neo4j agent

--- agents/neo4j_agent.py
+++ agents/neo4j_agent.py
@@ -11,21 +11,15 @@ from prompts.prompts import DEFAULT_PROMPT, SUMMARY_PROMPT
 import os
 
 NEO4J_URI = os.getenv("NEO4J_URI")
+PROMPT = "You are a sophisticated agent with a deep understanding of the world. You can answer questions and provide explanations. You can also ask questions and provide explanations.\nExisting knowledge is stored here: {existing_knowledge}"
 
 class Neo4jAgent(AbstractAgent):
 
     def __init__(self):
         # Initialize tools and memory
-        #neo_url = os.environ.get('NEO4J_URI')
-        #neo_url = 'bolt://neo4j:7687'
-        #self.neo = Neo4jWrapper(uri=neo_url, user='neo4j', password='password')
         self.logger = config.logger
         self.messages = []
-        self.prompt = "Your name is Sophia and you are an extremely knowledgable and capable assistant"
-        self.cypher_daemon = TextToCypherDaemon()
-        self.messages.append({"role": "system", "content": self.prompt})
-        self.last_input_message = None
-        self.last_response_message = None
+        self.messages.append({"role": "system", "content": PROMPT})
         self.memory = KGMemory()
    
     def append_message(self, message, role):
@@ -42,25 +36,14 @@ class Neo4jAgent(AbstractAgent):
     """
     def generate_query_sequence(self, text):
         try:
-            # Should go something like this:
-            # User input is used to extract relevant info from knowledge graph
-            # Extracted info is used to generate a response
-            # Response is returned to user
-            response = None
             self.append_message(text, "user")
-
             response = StaticOpenAIModel.generate_response(self.messages)
             response_dict = response.to_dict()
-            res = response_dict['choices'][0]['message']['content']
+            res = response['choices'][0]['message']['content']
             self.append_message(res, "assistant")
-            
             config.logger.debug(f"res: {res}")
-            #cypher_query = TextToCypherDaemon.generate_cypher(res)
-            #self.neo.query(cypher_query)
             self.memory.record(res)
-            #log_message = f"User query: {text}, Cypher query: {cypher_query}, Response: {response}"
             return res
 
         except Exception as e:
-            #self.logger.debug(e)
             return f"Exception returned: {e}"
